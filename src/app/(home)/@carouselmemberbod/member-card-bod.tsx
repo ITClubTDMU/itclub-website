4d572a6f2151adca20091755f6243868c96816af
nvminh155 <2124802010126@student.tdmu.edu.vn>
2025-01-08 14:08:30 +0700

    style(member): member icon gmail, fb click

--- src/app/(home)/@carouselmemberbod/member-card-bod.tsx
+++ src/app/(home)/@carouselmemberbod/member-card-bod.tsx
@@ -69,44 +69,37 @@ const MemberCardBOD = ({ className, member }: TMemberBODCardProps) => {
 
       <div className="flex items-center gap-3">
         {member.contact.map((c) => {
-          return (
-            <div
+          return c.type === "fb" ? (
+            <Link
               key={c.type}
-              className="h-max w-max cursor-pointer rounded-full p-4 leading-[0] hover:bg-gray-100"
+              href={c.link === "" ? "1" : "2"}
+              target="_blank"
+              className=" rounded-full p-4 !h-max  w-max hover:bg-gray-100"
+              onClick={(e) => {
+                if (c.isHidden) {
+                  e.preventDefault();
+                  toast.info("Facebook đã được ẩn", {
+                    className: "bg-primary",
+                    classNames: {
+                      icon: "text-primary",
+                    },
+                  });
+                }
+              }}
             >
-              {c.type === "fb" ? (
-                <Link
-                  key={c.type}
-                  href={c.link === "" ? "1" : "2"}
-                  target="_blank"
-                  className=""
-                  onClick={(e) => {
-                    if (c.isHidden) {
-                      e.preventDefault();
-                      toast.info("Facebook đã được ẩn", {
-                        className: "bg-primary",
-                        classNames: {
-                          icon: "text-primary",
-                        },
-                      });
-                    }
-                  }}
-                >
-                  <Facebook className="!h-6 !w-6 text-[#1877f2]" />
-                </Link>
-              ) : (
-                <Button
-                  key={c.type}
-                  variant={"ghost"}
-                  className={cn("h-max w-max p-0")}
-                  onClick={() => {
-                    handleCopy(c.link, c.isHidden);
-                  }}
-                >
-                  <Mail className="!h-6 !w-6 text-[#ee2054]" />
-                </Button>
-              )}
-            </div>
+              <Facebook className="!h-6 !w-6 text-[#1877f2]" />
+            </Link>
+          ) : (
+            <Button
+              key={c.type}
+              variant={"ghost"}
+              className={cn("h-max w-max rounded-full !p-4")}
+              onClick={() => {
+                handleCopy(c.link, c.isHidden);
+              }}
+            >
+              <Mail className="!h-6 !w-6 text-[#ee2054]" />
+            </Button>
           );
         })}
       </div>
